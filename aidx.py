b9e3d0bd9f53be7f8b67f76b50cc967e28e7e85b
OfficialDelta <51007646+TSLARoadster@users.noreply.github.com>
2024-01-21 16:04:27 -0500

    feat(aidx.py): Add commands for creating Parquet datasets and fine-tuning the model (main)

--- aidx.py
+++ aidx.py
@@ -3,6 +3,8 @@ import sys
 
 from src.main.create_mimicllm import main as create_mimicllm_main
 from src.main.tokenize_mimicllm import main as tokenize_mimicllm_main
+from src.main.create_parquet_datasets import main as create_parquet_datasets_main
+from src.main.fine_tune_model import main as fine_tune_model_main
 
 def main():
     parser = argparse.ArgumentParser(
@@ -37,6 +39,42 @@ def main():
         help="If set, will rewrite the log database",
     )
     tokenize_mimicllm_parser.set_defaults(func=tokenize_mimicllm_main)
+
+    create_parquet_datasets_parser = subparsers.add_parser("create-parquet-datasets", help="Create the Parquet train/test datasets")
+    create_parquet_datasets_parser.add_argument(
+        "--chunk-size",
+        type=int,
+        default=10000,
+        help="The chunk size to use for querying the database",
+    )
+    create_parquet_datasets_parser.add_argument(
+        "--test-size",
+        type=float,
+        default=0.2,
+        help="The test size to use for splitting the data",
+    )
+    create_parquet_datasets_parser.add_argument(
+        "--parquet-dir",
+        type=str,
+        default="data/parquet",
+        help="The directory to store the Parquet files",
+    )
+    create_parquet_datasets_parser.set_defaults(func=create_parquet_datasets_main)
+
+    fine_tune_model_parser = subparsers.add_parser("fine-tune-model", help="Fine tune the model")
+    fine_tune_model_parser.add_argument(
+        "--model-name",
+        type=str,
+        default="mistralai/Mixtral-8x7B-Instruct-v0.1",
+        help="The model name to use for fine tuning",
+    )
+    fine_tune_model_parser.add_argument(
+        "--parquet-dir",
+        type=str,
+        default="data/parquet",
+        help="The directory to store the Parquet files",
+    )
+    fine_tune_model_parser.set_defaults(func=fine_tune_model_main)
 
     args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
     args.func(args)
