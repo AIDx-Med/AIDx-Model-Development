d9a76cbbe052b36010bb9ea1c3f14d6e4a7cc0b4
OfficialDelta <51007646+TSLARoadster@users.noreply.github.com>
2024-01-23 09:32:40 -0500

    feat(fine_tune_model): Add bitsandbytes 4bit quantization, memory tracking

--- src/main/fine_tune_model.py
+++ src/main/fine_tune_model.py
@@ -1,13 +1,38 @@
 import numpy as np
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
+from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments, BitsAndBytesConfig
 from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model
 from datasets import load_dataset
 import os
 from evaluate import load
+from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo
 
 from src.processing.utils import transform_dataset_to_tensor, BatchPaddedCollator
 
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+def print_gpu_utilization():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    print(f"GPU memory occupied: {info.used//1024**2} MB.")
+
+def print_summary(result):
+    print(f"Time: {result.metrics['train_runtime']:.2f}")
+    print(f"Samples/second: {result.metrics['train_samples_per_second']:.2f}")
+    print_gpu_utilization()
 
 def main(args):
     model_name = args.model_name
@@ -15,14 +40,21 @@ def main(args):
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
     tokenizer.add_special_tokens({"pad_token": "[PAD]"})
 
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
+
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        load_in_4bit=True,
-        torch_dtype=torch.float16,
+        quantization_config=bnb_config,
         device_map="auto",
     )
 
     # Prepare model for k-bit training
+    model.gradient_checkpointing_enable()
     model = prepare_model_for_kbit_training(model)
 
     lora_r = 8
@@ -39,6 +71,7 @@ def main(args):
     )
 
     model = get_peft_model(model, config)
+    print_trainable_parameters(model)
 
     parquet_dir = args.parquet_dir
     train_parquet_file = os.path.join(parquet_dir, "train.parquet")
@@ -72,16 +105,20 @@ def main(args):
         args=TrainingArguments(
             per_device_train_batch_size=1,
             gradient_accumulation_steps=4,
+            warmup_steps=2,
             num_train_epochs=6,
-            learning_rate=1e-4,
-            logging_steps=2,
-            optim="adamw_torch",
+            learning_rate=2e-4,
+            logging_steps=1,
+            fp16=True,
+            optim="paged_adamw_8bit",
             output_dir="aidx-mixtral",
             load_best_model_at_end=True,
             compute_metrics=compute_metrics,
         ),
         data_collator=data_collator,
     )
+
+    model.config.use_cache = False
 
     train_result = trainer.train()
 
@@ -96,4 +133,6 @@ def main(args):
     eval_result = trainer.evaluate(eval_dataset=test_data, metric_key_prefix="test")
     trainer.log_metrics("test", eval_result)
     trainer.save_metrics("test", eval_result)
+
+    print_summary(train_result)
 
